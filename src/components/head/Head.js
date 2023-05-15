41ae8afc7f4f9ec53481c8f74ddd6490c76159f3
Amir Basirati <amirbasirati13@gmail.com>
2023-05-15 23:53:02 +0330

    shamsiCal created, calType initialized and can change with type change handler in Head component

--- src/components/head/Head.js
+++ src/components/head/Head.js
@@ -1,14 +1,20 @@
 import React from "react";
 import styles from "./Head.module.css";
 
-const Head = ({ setDate }) => {
+const Head = ({ setDate, calType, setCalType }) => {
   const setToday = () => {
     setDate(new Date());
+  };
+  const changeType = () => {
+    setCalType({ shamsi: !calType.shamsi, miladi: !calType.miladi });
   };
   return (
     <div className={styles.container}>
       <header>
-        <div>میلادی</div>
+        <div onClick={changeType}>
+          {calType.miladi && "تقویم شمسی"}
+          {calType.shamsi && "تقویم میلادی"}
+        </div>
         <div onClick={setToday}>امروز</div>
       </header>
       <hr />
